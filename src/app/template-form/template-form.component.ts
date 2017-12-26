79837fa02c8dae9fc18c9f790ccb38f1b2283c37
RubickCraft <429655198@qq.com>
2017-12-26 23:37:48 +0800

    添加select和radio表单控件

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -9,14 +9,26 @@ export class TemplateFormComponent implements OnInit {
 
   model: FormModel;
 
+  eduOptions: Array<{value: string, display: string}> = [
+    {value: 'xiaoxue', display: '小学'},
+    {value: 'chuzhong', display: '初中'},
+    {value: 'gaozhong', display: '高中'},
+    {value: 'daxue', display: '大学'},
+    {value: 'yanjiusheng', display: '研究生'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
-    this.model = new FormModel('username', 123, '123', '123', 123)
+    this.model = new FormModel('username', 123, '123', '123', 123, '', '')
   }
 
   onSubmit() {
     console.log(this.model);
+  }
+
+  selectChange() {
+    console.log(`下拉框选择的值==>${this.model.education}`);
   }
 
 }
@@ -27,6 +39,8 @@ export class FormModel {
     public mobile: number,
     public pwd: string,
     public confirmPwd: string,
-    public postCode: number
+    public postCode: number,
+    public sex: string,
+    public education: string
   ){}
 }
