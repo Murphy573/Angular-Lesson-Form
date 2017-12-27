0d5bf056598c07e6d7a47bd1d4f834add406326a
RubickCraft <429655198@qq.com>
2017-12-27 10:59:50 +0800

    给响应式表单添加class模型数据

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormModel} from '../template-form/template-form.component';
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,24 +8,6 @@ import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
-
-  formModel: FormGroup = new FormGroup({
-      userName: new FormControl('myusername', [
-        Validators.required,
-        Validators.minLength(4)
-      ]),
-      mobile: new FormControl(123456),
-      userPwd: new FormGroup({
-        pwd: new FormControl(123),
-        confirmPwd: new FormControl(456)
-      }),
-      sex: new FormControl('male'),
-      education: new FormControl('chuzhong'),
-      addresses: new FormArray([
-        new FormControl('地址一'),
-        new FormControl('地址二'),
-      ])
-  });
 
   eduOptions: Array<{value: string, display: string}> = [
     {value: 'xiaoxue', display: '小学'},
@@ -34,14 +17,6 @@ export class ReactiveFormComponent implements OnInit {
     {value: 'yanjiusheng', display: '研究生'},
   ];
 
-  selectChange() {
-    console.log(`下拉框选择的值==>${this.formModel.get('education').value}`);
-  }
-
-  onSubmit() {
-    console.log(this.formModel.value);
-  }
-
   //假如用户有多个手机号
   mobiles: FormArray = new FormArray([
     new FormControl(123),    new FormControl(456),
@@ -49,9 +24,43 @@ export class ReactiveFormComponent implements OnInit {
     new FormControl(236),
   ]);
 
+  model: FormModel;
+  formModel: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.model = new FormModel('username', 123, '123', '123', 123, 'male', 'chuzhong')
+    this.initFormGroup();
+  }
+
+  initFormGroup() {
+    this.formModel = new FormGroup({
+      userName: new FormControl(this.model.userName, [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      mobile: new FormControl(this.model.mobile),
+      userPwd: new FormGroup({
+        pwd: new FormControl(this.model.pwd),
+        confirmPwd: new FormControl(this.model.confirmPwd)
+      }),
+      sex: new FormControl(this.model.sex),
+      education: new FormControl(this.model.education),
+      addresses: new FormArray([
+        new FormControl('地址一'),
+        new FormControl('地址二'),
+      ])
+    });
+  }
+
+  selectChange() {
+    console.log(`下拉框选择的值==>${this.formModel.get('education').value}`);
+  }
+
+  onSubmit() {
+    console.log(this.formModel.value);
+    //console.log(this.model);
   }
 
   addAddress() {
